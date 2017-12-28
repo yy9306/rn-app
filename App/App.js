585a30496aba60e01e97c000424759ddb0f36728
军哥哥 <1273460656@qq.com>
2017-12-28 23:28:16 +0800

    go to page

--- App/App.js
+++ App/App.js
@@ -10,39 +10,6 @@ import ImageDetailBrower from './pages/movie/ImageDetailBrower'
 import MovieList from './pages/movie/MovieList'
 
 /*
-* 实现底部Tab
-* */
-const MainTabPage = TabNavigator({
-  Movie:{screen: Movie},
-},{
-  animationEnabled: true,
-  tabBarPosition: 'bottom',
-  swipeEnabled: true,
-  backBehavior: 'none',
-  tabBarOptions: {
-    // activeTintColor: MainColor,
-    // inactiveTintColor: GrayColor,
-    showIcon: true,
-    upperCaseLabel: false,
-    indicatorStyle: {
-      height: 0,
-    },
-    style: {
-      // backgroundColor: MainBg,
-      height:54,
-    },
-    labelStyle: {
-      padding:0,
-      margin:0,
-    },
-    iconStyle: {
-      padding:0,
-      margin:0,
-    }
-  }
-})
-
-/*
 * 实现跳转的栈
 * */
 const App = StackNavigator({
